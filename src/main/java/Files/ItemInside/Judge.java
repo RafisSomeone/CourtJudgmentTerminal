d8c9d1535d5222fd18ca89ace06d78867236da9e
RafisSomeone <rafal.lisak1@gmail.com>
2018-12-08 14:09:35 +0100

    dodana czwarta komenda

---
+++ src/main/java/Files/ItemInside/Judge.java
@@ -0,0 +1,16 @@
+package Files.ItemInside;
+
+import java.util.List;
+
+public class Judge {
+    private String name;
+    private String function;
+    private List<String> specialRoles;
+
+
+    public String toString() {
+        return this.name;
+    }
+
+
+}
