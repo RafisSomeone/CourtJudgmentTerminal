df3fe0472b7b097fa1c3d7b8f65975a48766c5b7
RafisSomeone <rafal.lisak1@gmail.com>
2018-12-10 12:01:41 +0100

    dodane StatisticJudges

--- src/main/java/Kernel/Main.java
+++ src/main/java/Kernel/Main.java
@@ -1,40 +1,83 @@
 package Kernel;
 
-import Commands.StatisticCourt;
-import Commands.StatisticJudges;
-import Commands.StatisticSentence;
-import Commands.TopRegulations;
-import Tools.DataBase;
+
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
+
 
 import java.io.IOException;
+
 
 public class Main {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        try {
-            DataBase mole = new DataBase();
-            mole.load("C:\\Users\\rafal\\IdeaProjects\\CourtJudgmentTerminal\\src\\main\\resources");
-            System.out.println(new StatisticJudges().statistic(mole));
-           //  System.out.println(new TopRegulations().topRegulation(mole));
-  //          System.out.println(new StatisticSentence().statistic(mole));
-        //    System.out.println(new StatisticCourt().statistic(mole));
-            //  System.out.println(new TopJudges().top10(mole));
-            // System.out.println("");
+      String prompt = "prompt> ";
+      Terminal terminal = TerminalBuilder.builder()
+              .system(true)
+              .build();
 
-            //System.out.println(new FindRubrum().searchFor("VIII Ka 797/13", mole));
-            //System.out.println(new Converter().clean(new FindContent().searchForContent("VIII Ka 797/13", mole)));
-            //System.out.println(new FindMore().searchForMore("VIII Ka 797/13,V Pa 51/13,IV CZ 132/13", mole));
-            //System.out.println(new JudgeCaseCounter().howMany("Wojciech Katner",mole));
-            String out = "MAMA";
+      LineReader lineReader= LineReaderBuilder.builder()
+              .terminal(terminal)
+              .build();
 
-            //  TerminalBuilder terminal = new TerminalBuilder();
+String line ;
+line = lineReader.readLine(prompt);
 
-        } catch (IOException ex) {
-            System.out.println(ex);
 
-        }
+/*        try {
+
+            //DataBase mole = new DataBase();
+          //  mole.load("C:\\Users\\rafal\\IdeaProjects\\CourtJudgmentTerminal\\src\\main\\resources");
+            // System.out.println(new StatisticJudges().statistic(mole));
+
+
+            String prompt = "prompt> ";
+            String rightPrompt = null;
+           Terminal terminal = TerminalBuilder.builder()
+                    .system(true)
+                    .build();
+
+            LineReader reader = LineReaderBuilder.builder()
+                    .terminal(terminal)
+                    .build();
+            while (true) {
+                String line = null;
+                try {
+                    line = reader.readLine(prompt);
+                    if(line=="asd") System.out.println("FSAF");
+
+                } catch (UserInterruptException e) {
+                    System.out.println("fdgsfdgsd");
+                } catch (EndOfFileException e) {
+
+                }
+
+            }
+                //  System.out.println(new TopRegulations().topRegulation(mole));
+                //          System.out.println(new StatisticSentence().statistic(mole));
+                //    System.out.println(new StatisticCourt().statistic(mole));
+                //  System.out.println(new TopJudges().top10(mole));
+                // System.out.println("");
+
+                //System.out.println(new FindRubrum().searchFor("VIII Ka 797/13", mole));
+                //System.out.println(new Converter().clean(new FindContent().searchForContent("VIII Ka 797/13", mole)));
+                //System.out.println(new FindMore().searchForMore("VIII Ka 797/13,V Pa 51/13,IV CZ 132/13", mole));
+                //System.out.println(new JudgeCaseCounter().howMany("Wojciech Katner",mole));
+
+
+                //  TerminalBuilder terminal = new TerminalBuilder();
+
+            } catch(IOException ex){
+                System.out.println(ex);
+
+            }
+*/
+
     }
 }
 
